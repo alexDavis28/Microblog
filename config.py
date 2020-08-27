657e3fb843cad3f8a1097865a23cb0f918678aeb
Alex Davis <8m3xlt@gmail.com>
2020-08-27 17:02:52 +0100

    Password reset system

--- config.py
+++ config.py
@@ -14,6 +14,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['8m3xlt@gmail.com']
+    ADMINS = ["justsomeonenamedalexdev@gmail.com", '8m3xlt@gmail.com']
 
     POSTS_PER_PAGE = 25
