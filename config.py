b5af2a81bd762989b799c38e6a8bba2c316e7f6b
Alex Davis <8m3xlt@gmail.com>
2020-08-27 15:42:55 +0100

    Simple pagination

--- config.py
+++ config.py
@@ -15,3 +15,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['8m3xlt@gmail.com']
+
+    POSTS_PER_PAGE = 3
