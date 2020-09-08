0720bd2a72db4b9c69927fe60b52b188eaafee38
Alex Davis <8m3xlt@gmail.com>
2020-09-08 18:34:17 +0100

    repl.it support

--- microblog.py
+++ microblog.py
@@ -5,3 +5,8 @@ from app.models import User, Post
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == '__main__':
+    # For hosting on repl.it
+    app.run(host='0.0.0.0', port=8080)
